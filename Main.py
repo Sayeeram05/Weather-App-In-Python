b298488792334d0e38b57c66a5f2955ecac08f4b
Sayeeram05 <sr6172@srmist.edu.in>
2024-03-29 23:52:01 +0530

    modified Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from turtle import bgcolor
 import Data 
 Window = Tk()
 
@@ -25,36 +26,38 @@ City.place(x=540 - City.winfo_reqwidth()//2,y=28)
 
 #Button
 
+
 def GetCity():
     CityName = City.get()
    
     WeatherData = list(Data.CollectWeatherInfo(City=CityName))
     print(WeatherData)
 
-    WeatherLabel = Label(Window,text=WeatherData[0],font=("Arial",45),bg="#c0c0c0",width=100,justify="center")
+    
+    WeatherLabel.config(text=WeatherData[0])
     WeatherLabel.place(x=540 - WeatherLabel.winfo_reqwidth()//2,y=100)
 
-    TempLabel = Label(Window,text=WeatherData[1],font=("Arial",30),bg="#c0c0c0",width=15,justify="center")
+    TempLabel.config(text=WeatherData[1])
     TempLabel.place(x=540 - TempLabel.winfo_reqwidth()//2,y=180)
 
-    MinTempLabel = Label(Window,text=WeatherData[2],font=("Arial",20),bg="#c0c0c0",width=15,justify="center")
+    MinTempLabel.config(text=WeatherData[2])
     MinTempLabel.place(x=540 - MinTempLabel.winfo_reqwidth()//2 - 350,y=550)
 
-    HumidityLabel = Label(Window,text=WeatherData[4],font=("Arial",20),bg="#c0c0c0",width=15,justify="center")
+    HumidityLabel.config(text=WeatherData[4])
     HumidityLabel.place(x=540 - HumidityLabel.winfo_reqwidth()//2,y=550) 
 
-    MaxTempLabel = Label(Window,text=WeatherData[3],font=("Arial",20),bg="#c0c0c0",width=15,justify="center")
+    MaxTempLabel.config(text=WeatherData[3])
     MaxTempLabel.place(x=540 - MaxTempLabel.winfo_reqwidth()//2 + 350,y=550)
     
 
 SearchButton = Button(Window,text="Search",padx=10,font=("Arial",17),command=GetCity)
 SearchButton.place(x=540 + City.winfo_reqwidth()//2 + 25,y=28)
 
-TodaysWeatherLabel = Label(Window,text="Todays Weather",font=("Arial",45),bg="#c0c0c0")
-TodaysWeatherLabel.place(x=540 - TodaysWeatherLabel.winfo_reqwidth()//2,y=100)
+WeatherLabel = Label(Window,text="Weather",font=("Arial",45),bg="#c0c0c0")
+WeatherLabel.place(x=540 - WeatherLabel.winfo_reqwidth()//2,y=100)
 
-TodaysTempLabel = Label(Window,text="--.--",font=("Arial",30),bg="#c0c0c0")
-TodaysTempLabel.place(x=540 - TodaysTempLabel.winfo_reqwidth()//2,y=180)
+TempLabel = Label(Window,text="--.--",font=("Arial",30),bg="#c0c0c0")
+TempLabel.place(x=540 - TempLabel.winfo_reqwidth()//2,y=180)
 
 MinTempLabel = Label(Window,text="Min Temp",font=("Arial",20),bg="#c0c0c0")
 MinTempLabel.place(x=540 - MinTempLabel.winfo_reqwidth()//2 - 350,y=500) 
@@ -65,14 +68,14 @@ HumidityLabel.place(x=540 - HumidityLabel.winfo_reqwidth()//2,y=500)
 MaxTempLabel = Label(Window,text="Max Temp",font=("Arial",20),bg="#c0c0c0")
 MaxTempLabel.place(x=540 - MaxTempLabel.winfo_reqwidth()//2 + 350,y=500) 
 
-TodaysMinTempLabel = Label(Window,text="--.--",font=("Arial",20),bg="#c0c0c0")
-TodaysMinTempLabel.place(x=540 - TodaysMinTempLabel.winfo_reqwidth()//2 - 350,y=550)
+MinTempLabel = Label(Window,text="--.--",font=("Arial",20),bg="#c0c0c0")
+MinTempLabel.place(x=540 - MinTempLabel.winfo_reqwidth()//2 - 350,y=550)
 
-TodaysHumidityLabel = Label(Window,text="--.--",font=("Arial",20),bg="#c0c0c0")
-TodaysHumidityLabel.place(x=540 - TodaysHumidityLabel.winfo_reqwidth()//2,y=550) 
+HumidityLabel = Label(Window,text="--.--",font=("Arial",20),bg="#c0c0c0")
+HumidityLabel.place(x=540 - HumidityLabel.winfo_reqwidth()//2,y=550) 
 
-TodaysMaxTempLabel = Label(Window,text="--.--",font=("Arial",20),bg="#c0c0c0")
-TodaysMaxTempLabel.place(x=540 - TodaysMaxTempLabel.winfo_reqwidth()//2 + 350,y=550)
+MaxTempLabel = Label(Window,text="--.--",font=("Arial",20),bg="#c0c0c0")
+MaxTempLabel.place(x=540 - MaxTempLabel.winfo_reqwidth()//2 + 350,y=550)
 
 
 Window.mainloop()
